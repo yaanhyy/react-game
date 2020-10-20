b3714526e025ad34937796a38105ccb26664e084
hongyuanyang <hongyuanyang@huobi.com>
2020-10-20 11:43:04 +0800

    not use function react

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Game from './App';
+import {APP} from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
